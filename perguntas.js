9f0bad93bf74c575f5adc123ea3fbdb8b24a3ef4
analuizasteil <148882755+analuizasteil@users.noreply.github.com>
2024-10-22 11:55:32 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'a pergunta',
+    'quantas vezes devemos perdoar nossos irmãos?',
+    'Jesus respondeu: “Não te digo até sete vezes, mas até setenta vezes sete.'
+)
+
+criaCartao(
+    'pecados',
+    'quais eram os pecados que jesus detesta?',
+    ' o olhar orgulhoso, a língua mentirosa, mãos que matam gente inocente, a mente que faz planos perversos...'
+)
+
+criaCartao(
+    'JESUS',
+    'quem é jesus?',
+    'Ele foi o Grande Jeová do Velho Testamento e o Messias do Novo Testamento.'
+)
+
+criaCartao(
+    'nascimento',
+    'onde jesus nasceu ?',
+    'Na cidade de Belém, na Cisjordânia,'
+)
